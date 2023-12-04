48b447c241a747037f3fb5216de319d17d11716a
ory98 <subin8360@naver.com>
2023-12-04 19:33:33 +0900

    init: CI/CD 배포 파이프라인 구축 - 웹서버 생성

---
+++ README.md
@@ -0,0 +1,5 @@
+# 항해99 프로젝트 
+
+현재 작업 진행 : 1주차 - Step1.웹서버생성
+- spring actuator 추가 (endPoint : /actuator/health)
+- 서버 Phase 별 yml파일 분리 (develop/staging/product)
---
+++ src/test/java/kr/hanghae/deploy/DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hanghae.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
