ffe3735f6d6d7b188eee89bdc819d7c856d73dc9
ory98 <subin8360@naver.com>
2023-12-29 16:26:41 +0900

    - OrderProductRepository.java 삭제

--- src/main/java/kr/hanghae/deploy/domain/orderproduct/OrderProductRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.hanghae.deploy.domain.orderproduct;
-
-import kr.hanghae.deploy.domain.orderproduct.OrderProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
-
-}
