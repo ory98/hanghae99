f5cd90f8b93cbaf361cfe143affd754dfa43eb12
ory98 <subin8360@naver.com>
2024-01-12 23:59:57 +0900

    낙관적 lock: repository에 작성

--- src/main/java/kr/hanghae/deploy/domain/user/UserRepository.java
+++ src/main/java/kr/hanghae/deploy/domain/user/UserRepository.java
@@ -1,11 +1,14 @@
 package kr.hanghae.deploy.domain.user;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    @Lock(value = LockModeType.OPTIMISTIC)
     Optional<User> findByUsername(String username);
 
 }
